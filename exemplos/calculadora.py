746d60b0a8dde88c38b6ad65f18120cf74766ae7
WellingtonPereiraLuiz <wellingtonpereiraluiz89@gmail.com>
2024-08-27 21:17:10 -0400

    desafio

---
+++ exemplos/calculadora.py
@@ -0,0 +1,15 @@
+from tkinter import *
+import tkinter.font as tkfont
+
+app = Tk()
+app.title('Análise e Desenvolvimento de Sistemas')
+app.configure(background='#10c7e5')
+app.grid_rowconfigure(0, weight=4)
+app.grid_rowconfigure(1, weight=6)
+app.grid_columnconfigure(0, weight=1)
+
+
+
+
+
+app.mainloop()
