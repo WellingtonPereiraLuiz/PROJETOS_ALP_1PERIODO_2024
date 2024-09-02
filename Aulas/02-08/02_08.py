aa0f90f69e9e56220ce61641e29d9b7d7576a242
wellington pereira luiz <108313369+WellingtonPereiraLuiz@users.noreply.github.com>
2024-09-01 22:31:12 -0400

    exemplos do material

---
+++ Aulas/02-08/02_08.py
@@ -0,0 +1,22 @@
+from collections import Counter
+"""
+lista = ['Márcia','Márcia','Alice','Antonio','Antonio','Pedro','Pedro']
+
+print(f'\nResultado : {Counter(lista)}')
+
+print(f'\nO tipo do resultado é: {type(Counter(lista))}')
+
+
+lista = [1,1,1,2,2,2,3,3,4,4,4,5,3,45,45,66,42,36,105.19,105.19]
+
+print(f'\nResultado : {Counter(lista)}')
+
+print(f'\nO tipo do resultado é: {type(Counter(lista))}')
+
+"""
+
+frase = ['IFRO Campus Ariquemes']
+
+print(f'\nResultado : {Counter(frase)}')
+
+print(f'\nO tipo do resultado é: {type(Counter(frase))}')
