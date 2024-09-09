ac23d0a9de749ee3d92dece028fbf0df4fceeb2f
WellingtonPereiraLuiz <wellingtonpereiraluiz89@gmail.com>
2024-09-09 20:49:03 -0300

    exemplos

---
+++ Aulas/09-09/exemplo09_09.py
@@ -0,0 +1,7 @@
+while True:
+    try:
+        a = int(input('Digite sua idade:'))
+        if a == 0:
+            break
+    except(ValueError):
+        print('Seu burro é para digitar numeros inteiros')
